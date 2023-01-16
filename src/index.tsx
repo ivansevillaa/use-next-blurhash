38547053cc82d8fd8bb27004766a91a5fd7a843b
Bezirgen <anonimanonimus955@gmail.com>
2023-01-16 14:17:42 +0500

    Canvas removed.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import { decode } from "blurhash";
-import { createCanvas } from "canvas";
+import blurHashToDataURL from "./blurHashToDataUrl";
 
 export default function useBlurData(
   blurHash: string,
@@ -7,13 +6,5 @@ export default function useBlurData(
   height: number = 120,
   punch?: number
 ) {
-  const pixels = decode(blurHash, width, height, punch);
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
-  const imageData = ctx.createImageData(width, height);
-  imageData.data.set(pixels);
-  ctx.putImageData(imageData, 0, 0);
-  const blurDataUrl = canvas.toDataURL();
-
-  return [blurDataUrl];
+  return blurHashToDataURL(blurHash, width, height, punch);
 }
